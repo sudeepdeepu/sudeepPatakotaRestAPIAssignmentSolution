ad36f7a495f2c08d5552cc049821e959b25b0851
sudeepdeepu <sudep1997@gmail.com>
2022-04-11 10:18:57 +0530

    completed the coding

---
+++ Employee-Service-Rest-Security/target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into roles(role_id,name) values('1','ADMIN');
+insert into users(user_id,password,username) values('1','1$2a$12$.1iwnSMKZz97wd93jePAuW4rl3Gxz3fjnhPL2OshsnuTuOPskn','ADMIN');
+insert into users_roles(user_id,role_id) values('1','1');
